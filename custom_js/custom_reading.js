c16949d2b25d92c3680a0b3f59a98afeb60af8d0
chanasorn1234 <62010153@kmitl.ac.th>
2022-04-26 18:13:10 +0700

    add page-reading

---
+++ custom_js/custom_reading.js
@@ -0,0 +1,27 @@
+// const container = document.getElementById('book-detail-container')
+// var add_height = 100
+
+// for(var i=0;i<=10;i++){
+//     add_height += 100
+//     const page = document.querySelector('.mother-page-book')
+//     // const container = document.getElementById('book-detail-container')
+//     const add_page = document.createElement('li')
+//     const img = document.createElement('img')
+//     img.classList.add('page-book')
+//     img.src = 'https://www.cp.eng.chula.ac.th/books/wp-content/uploads/sites/5/2018/08/python_cover_v2.png'
+//     add_page.append(img)
+//     page.appendChild(add_page)
+//     container.style.height = String(add_height)+"vh"
+//     // console.log(container.style.height)
+//     // console.log(String(add_height))
+// }
+
+// const back = document.getElementById('back')
+// back.addEventListener('click',function(){
+//     window.location.href = '#'
+// })
+
+// const save_page = document.getElementById('save-page')
+// save_page.addEventListener('click',function(){
+//    console.log(window.scrollY)    
+// })
